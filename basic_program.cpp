e9e4372df063ce848a988710c355fc50b40acf9d
M N Sakib <m.n.sakib@brainstation-23.com>
2024-04-25 12:31:56 +0600

    committing for rebase

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,13 +1,21 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+int fib(int n)
+{
+    int a = 0, b = 1, c, i;
+    if (n == 0)
+        return a;
+    for (i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
 int main() {
-    std::cout << "Hello World!";
-    cout << "Dhaka"<<endl;
-    cout << "BS23"<<endl;
-    cout<<"Creating Conflict in main"<<endl;
-    
-    cout<<"creating conflict in feature branch"<<endl;
-    
+   int n = 9;
+ 
+    cout << fib(n);
 
     return 0;
 }
